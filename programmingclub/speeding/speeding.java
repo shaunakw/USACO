45e6194f9634415a867c9b42de5bdb32e9120d6e
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2020-10-29 11:52:07 -0700

    Programming club 10/29

---
+++ programmingclub/speeding/speeding.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class speeding {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("speeding.in"));
+        PrintWriter out = new PrintWriter("speeding.out");
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        int[] segmentLength = new int[N];
+        int[] segmentLimit = new int[N];
+        int[] cowLength = new int[M];
+        int[] cowSpeed = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            segmentLength[i] = in.nextInt();
+            segmentLimit[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < M; i++) {
+            cowLength[i] = in.nextInt();
+            cowSpeed[i] = in.nextInt();
+        }
+
+        int segment = 0; // current road segment
+        int segmentStart = 0; // start of the current road segment
+        int cow = 0; // current cow segment
+        int cowStart = 0; // start of the current cow segment
+        int speeding = 0; // max over speed limit
+        for (int i = 0; i < 100; i++) {
+            if (segmentStart + segmentLength[segment] == i) {
+                segmentStart = i;
+                segment++;
+            }
+            if (cowStart + cowLength[cow] == i) {
+                cowStart = i;
+                cow++;
+            }
+            speeding = Math.max(speeding, cowSpeed[cow] - segmentLimit[segment]);
+        }
+
+        out.println(speeding);
+        out.close();
+    }
+}
