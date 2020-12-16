679b7a4b28c507530d456259318b233085ed7d5b
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2020-12-16 11:25:27 -0800

    Add topological sort, move algorithms to separate folder.

---
+++ algorithms/topsort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> topologicalSort(vector<vector<int>>& adjList) {
+    int size = adjList.size();
+    vector<int> inDegrees(size, 0);
+
+    for (auto& i : adjList) {
+        for (auto& j : i) inDegrees[j]++;
+    }
+
+    queue<int> q;
+
+    for (int i = 0; i < size; i++) {
+        if (inDegrees[i] == 0) q.push(i);
+    }
+
+    vector<int> sorted;
+    while (!q.empty()) {
+        int top = q.front();
+        q.pop();
+        sorted.push_back(top);
+
+        for (int& i : adjList[top]) {
+            inDegrees[i]--;
+            if (inDegrees[i] == 0) q.push(i);
+        }
+    }
+
+    return sorted;
+}
